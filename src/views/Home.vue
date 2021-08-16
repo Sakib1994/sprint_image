eda13654e20ae2e3514883a96f023cd8c94f5f6a
Sakib1994 <mdnazmussakibcse@gmail.com>
2021-08-16 10:46:40 +0600

    show image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,73 +12,114 @@
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="item in desserts" :key="item.name">
-                  <td>{{ item.name }}</td>
-                  <td>{{ item.calories }}</td>
+                <tr v-for="item in products" :key="item.Name">
+                  <td>{{ item.Name }}</td>
+                  <td>
+                    <img
+                      :src="baseUrl + item.Image.formats.thumbnail.url"
+                      :alt="item.Name"
+                      :height="item.Image.formats.thumbnail.height"
+                    />
+                  </td>
                 </tr>
               </tbody>
             </template>
           </v-simple-table>
-        </v-col></v-row
-      ></v-container
-    >
+        </v-col>
+        <v-col sm="6">
+          <v-form>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="name"
+                    label="Outlined"
+                    outlined
+                    clearable
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-file-input
+                    chips
+                    multiple
+                    outlined
+                    label="File input w/ chips"
+                  ></v-file-input>
+                </v-col>
+                <v-col cols="12">
+                  <v-btn class="mr-4" type="submit"> submit </v-btn>
+                  <v-btn @click="clear"> clear </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
   name: "Home",
-  // components: {
-  //   HelloWorld,
-  // },
-  data() {
-    return {
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-        },
-        {
-          name: "Donut",
-          calories: 452,
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-        },
-      ],
-    };
+  methods: {
+    clear() {},
+  },
+  data: () => ({
+    name: "",
+    desserts: [
+      {
+        name: "Frozen Yogurt",
+        calories: 159,
+      },
+      {
+        name: "Ice cream sandwich",
+        calories: 237,
+      },
+      {
+        name: "Eclair",
+        calories: 262,
+      },
+      {
+        name: "Cupcake",
+        calories: 305,
+      },
+      {
+        name: "Gingerbread",
+        calories: 356,
+      },
+      {
+        name: "Jelly bean",
+        calories: 375,
+      },
+      {
+        name: "Lollipop",
+        calories: 392,
+      },
+      {
+        name: "Honeycomb",
+        calories: 408,
+      },
+      {
+        name: "Donut",
+        calories: 452,
+      },
+      {
+        name: "KitKat",
+        calories: 518,
+      },
+    ],
+    products: null,
+    baseUrl: "http://localhost:1337",
+  }),
+  mounted() {
+    axios.get(this.baseUrl + "/products").then((response) => {
+      this.products = response.data;
+      console.log(response.data);
+    });
   },
 };
 </script>
